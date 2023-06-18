f6bc1ca7f5d5c88076f7e8b444bb14a885a50138
xprauns1 <xprauns1@mendelu.cz>
2023-06-18 17:48:20 +0200

    adding db

--- WA_BE/app.js
+++ WA_BE/app.js
@@ -1,18 +1,25 @@
+//SETUP
 const express = require('express');
 const app = express();
+const {initializeDatabase} = require('./database/database');
 const jwt = require('express-jwt');
 const {jwtConfig} = require("./config");
 const config = require("./config");
 
 //routes
 const homepageRouter = require('./routes/homepage');
+
 //FE connection via cors
+
+//set up everything
+initializeDatabase();
+app.use(express.json());
 
 // register all routes
 app.use('/', homepageRouter);
 
 
-app.use(express.json());
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
